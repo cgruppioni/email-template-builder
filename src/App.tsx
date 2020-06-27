b4c07a44ee14ee48fa149951629d81d7f472ebab
Casey Gruppioni <cgruppioni@gmail.com>
2020-06-27 16:17:57 -0400

    Comment out icon credit for now

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App = () => {
       </Formik>
       </section>
       <footer>
-        <div>Icons made by <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        {/* <div>Icons made by <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
     </footer>
     </div>
   );
