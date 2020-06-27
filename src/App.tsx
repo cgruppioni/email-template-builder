52bd7a32f04bf6aadfa97550962b45f0a8b479b2
Casey <cgruppioni@users.noreply.github.com>
2020-06-27 17:02:13 -0400

    Template Form can submit. Add linting  (#4)
    * yarn add eslint
    
    * yarn install yup and eslint depedencies
    
    * Move TemplateForm to seperate component out of App file
    
    * TemplateForm can submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import React from 'react'
 
-import mail from './mail.svg';
-import './App.css';
-
-
+import './App.css'
+import { TemplateForm } from './TemplateForm/index'
+import mail from './mail.svg'
 
 const App = () => {
   return (
@@ -13,54 +11,13 @@ const App = () => {
         <img src={mail} className="App-logo" alt="logo" />
       </header>
       <section>
-      <Formik
-        initialValues={{ mailTo: '', cc: '', bcc: '', subject: '', body: '' }}
-        validate={values => {
-          const errors = {};
-          if (!values.mailTo) {
-            errors.mailTo = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.mailTo)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
-        onSubmit={(values, {setSubmitting}) => {
-          console.log("ASDASD")
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-              <label>Mail To: </label>
-              <Field type="mailTo" name="mailTo" />
-              <ErrorMessage name="mailTo" component="div" />
-              <label>cc: </label>
-              <Field type="cc" name="cc" />
-              <ErrorMessage name="cc" component="div" />
-              <label>bcc: </label>
-              <Field type="bcc" name="bcc" />
-              <ErrorMessage name="bcc" component="div" />
-              <label>subject: </label>
-              <Field type="subject" name="subject" as="textarea" />
-              <ErrorMessage name="subject" component="div" />
-              <label>body: </label>
-              <Field type="body" name="body" as="textarea" />
-              <ErrorMessage name="body" component="div" />
-            <button type="submit" disabled={isSubmitting}>Submit</button>
-          </Form>
-        )}
-      </Formik>
+        <TemplateForm />
       </section>
       <footer>
         {/* <div>Icons made by <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
     </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
