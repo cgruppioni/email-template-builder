9ea91f1edae6a41d1b263531c99e726bf41cd828
Casey Gruppioni <cgruppioni@gmail.com>
2020-06-30 16:30:39 -0400

    Rename HTML element

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ const App = () => {
       <header className="App-header">
         <img src={mail} className="App-logo" alt="logo" />
       </header>
-      <section>
+      <main>
         <TemplateForm />
-      </section>
+      </main>
       <footer>
         {/* <div>Icons made by <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
     </footer>
