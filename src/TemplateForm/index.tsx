d4a96e6f00667ba2dd392c3853000daaa3bd305b
Casey Gruppioni <cgruppioni@gmail.com>
2020-06-30 16:34:32 -0400

    Add p tag around each form element

--- src/TemplateForm/index.tsx
+++ src/TemplateForm/index.tsx
@@ -43,54 +43,62 @@ export const TemplateForm = () => {
             <div>{formik.errors.mailTo}</div>
           ) : null}
         </p>
-        <label htmlFor="cc">cc</label>
-        <input
-          id="cc"
-          name="cc"
-          type="text"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.cc}
-        />
-        {formik.touched.cc && formik.errors.cc ? (
-          <div>{formik.errors.cc}</div>
-        ) : null}
-        <label htmlFor="bcc">Bcc</label>
-        <input
-          id="bcc"
-          name="bcc"
-          type="bcc"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.bcc}
-        />
-        {formik.touched.bcc && formik.errors.bcc ? (
-          <div>{formik.errors.bcc}</div>
-        ) : null}
-        <label htmlFor="subject">Subject</label>
-        <input
-          id="subject"
-          name="subject"
-          type="subject"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.subject}
-        />
-        {formik.touched.subject && formik.errors.subject ? (
-          <div>{formik.errors.subject}</div>
-        ) : null}
-        <label htmlFor="body">Body</label>
-        <input
-          id="body"
-          name="body"
-          type="body"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.body}
-        />
-        {formik.touched.body && formik.errors.body ? (
-          <div>{formik.errors.body}</div>
-        ) : null}
+        <p>
+          <label htmlFor="cc">cc</label>
+          <input
+            id="cc"
+            name="cc"
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.cc}
+          />
+          {formik.touched.cc && formik.errors.cc ? (
+            <div>{formik.errors.cc}</div>
+          ) : null}
+        </p>
+        <p>
+          <label htmlFor="bcc">Bcc</label>
+          <input
+            id="bcc"
+            name="bcc"
+            type="bcc"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.bcc}
+          />
+          {formik.touched.bcc && formik.errors.bcc ? (
+            <div>{formik.errors.bcc}</div>
+          ) : null}
+        </p>
+        <p>
+          <label htmlFor="subject">Subject</label>
+          <input
+            id="subject"
+            name="subject"
+            type="subject"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.subject}
+          />
+          {formik.touched.subject && formik.errors.subject ? (
+            <div>{formik.errors.subject}</div>
+          ) : null}
+        </p>
+        <p>
+          <label htmlFor="body">Body</label>
+          <input
+            id="body"
+            name="body"
+            type="body"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.body}
+          />
+          {formik.touched.body && formik.errors.body ? (
+            <div>{formik.errors.body}</div>
+          ) : null}
+        </p>
         <button type="submit">Submit</button>
       </form>
       <p>{formResponse}</p>
