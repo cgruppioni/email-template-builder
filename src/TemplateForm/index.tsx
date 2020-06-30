24c4d8109b69e8a974870b0760d11334d10e567e
Casey Gruppioni <cgruppioni@gmail.com>
2020-06-30 17:00:47 -0400

    Formik uses React Context syntax

--- src/TemplateForm/index.tsx
+++ src/TemplateForm/index.tsx
@@ -1,107 +1,49 @@
 import React, { useState } from 'react'
-import { useFormik } from 'formik'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 
 export const TemplateForm = () => {
-  const [formResponse, setFormResponse] = useState('')
+    const [formResponse, setFormResponse] = useState('')
 
-  const formik = useFormik({
-    initialValues: {
-      mailTo: '',
-      cc: '',
-      bcc: '',
-      subject: '',
-      body: '',
-    },
-    validationSchema: Yup.object({
-      mailTo: Yup.string()
-        .required('Required'),
-      subject: Yup.string()
-        .required('Required'),
-      body: Yup.string()
-        .required('Required')
-    }),
-    onSubmit: values => {
-      setFormResponse('You successfully submit the form!')
-      // alert(JSON.stringify(values, null, 2))
-    },
-  })
-  return (
-    <>
-      <form onSubmit={formik.handleSubmit}>
-        <p>
-          <label htmlFor="mailTo">Mail To</label>
-          <input
-            id="mailTo"
-            name="mailTo"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.mailTo}
-          />
-          {formik.touched.mailTo && formik.errors.mailTo ? (
-            <div>{formik.errors.mailTo}</div>
-          ) : null}
-        </p>
-        <p>
-          <label htmlFor="cc">cc</label>
-          <input
-            id="cc"
-            name="cc"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.cc}
-          />
-          {formik.touched.cc && formik.errors.cc ? (
-            <div>{formik.errors.cc}</div>
-          ) : null}
-        </p>
-        <p>
-          <label htmlFor="bcc">Bcc</label>
-          <input
-            id="bcc"
-            name="bcc"
-            type="bcc"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.bcc}
-          />
-          {formik.touched.bcc && formik.errors.bcc ? (
-            <div>{formik.errors.bcc}</div>
-          ) : null}
-        </p>
-        <p>
-          <label htmlFor="subject">Subject</label>
-          <input
-            id="subject"
-            name="subject"
-            type="subject"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.subject}
-          />
-          {formik.touched.subject && formik.errors.subject ? (
-            <div>{formik.errors.subject}</div>
-          ) : null}
-        </p>
-        <p>
-          <label htmlFor="body">Body</label>
-          <input
-            id="body"
-            name="body"
-            type="body"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.body}
-          />
-          {formik.touched.body && formik.errors.body ? (
-            <div>{formik.errors.body}</div>
-          ) : null}
-        </p>
-        <button type="submit">Submit</button>
-      </form>
-      <p>{formResponse}</p>
-    </>
-  )
+    return (
+        <>
+            <Formik
+                initialValues={{ mailTo: '', subject: '', body: '' }}
+                validationSchema={Yup.object({
+                    mailTo: Yup.string()
+                        .max(15, 'Must be 15 characters or less')
+                        .required('Required'),
+                    subject: Yup.string()
+                        .max(15, 'Must be 15 characters or less')
+                        .required('Required'),
+                    body: Yup.string()
+                        .max(15, 'Must be 15 characters or less')
+                        .required('Required'),
+                })}
+                onSubmit={(values, { setSubmitting }) => {
+                    setFormResponse('You successfully submit the form!')
+                }}
+            >
+                <Form>
+                    <label htmlFor="mailTo">Mail To</label>
+                    <Field name="mailTo" type="text" />
+                    <ErrorMessage name="mailTo" />
+                    <label htmlFor="cc">Last Name</label>
+                    <Field name="cc" type="text" />
+                    <ErrorMessage name="cc" />
+                    <label htmlFor="bcc">Email Address</label>
+                    <Field name="bcc" type="bcc" />
+                    <ErrorMessage name="bcc" />
+                    <label htmlFor="subject">Subject</label>
+                    <Field name="subject" type="text" />
+                    <ErrorMessage name="subject" />
+                    <label htmlFor="body">Body</label>
+                    <Field name="body" type="text" />
+                    <ErrorMessage name="body" />
+                    <button type="submit">Submit</button>
+                </Form>
+            </Formik>
+            <p>{formResponse}</p>
+        </>
+    )
 }
