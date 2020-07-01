9d8d87a934445db1d007c0062bc6bacf2c6e5285
Casey Gruppioni <cgruppioni@gmail.com>
2020-06-30 22:37:41 -0400

    Form submits and returns success message

--- src/TemplateForm/index.tsx
+++ src/TemplateForm/index.tsx
@@ -12,107 +12,99 @@ const schema = Yup.object({
 })
 
 export const TemplateForm = () => {
-    // const [formResponse, setFormResponse] = useState('')
+    const [formResponse, setFormResponse] = useState('')
 
     return (
-        <Formik
-            validationSchema={schema}
-            onSubmit={(values) => {console.log('hi')}}
-            initialValues={{
-                mailTo: '',
-                cc: '',
-                bcc: '',
-                subject: '',
-                body: '',
-            }}
-        >
-            {({
-                handleSubmit,
-                handleChange,
-                handleBlur,
-                values,
-                touched,
-                isValid,
-                errors,
-            }) => (
-                <Form noValidate onSubmit={handleSubmit}>
-                    <Form.Row>
-                        <Form.Group as={Col} md="4" controlId="validationFormik01">
-                            <Form.Label>Mail to</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="mailTo"
-                                value={values.mailTo}
-                                onChange={handleChange}
-                                isValid={touched.mailTo && !errors.mailTo}
-                            />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group as={Col} md="4" controlId="validationFormik02">
-                            <Form.Label>cc</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="cc"
-                                value={values.cc}
-                                onChange={handleChange}
-                                isValid={touched.cc && !errors.cc}
-                            />
-
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group as={Col} md="4" controlId="validationFormikUsername">
-                            <Form.Label>bcc</Form.Label>
-                            <Form.Group as={Col} md="6" controlId="validationFormik03">
-                                <Form.Label>bcc</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder=""
-                                    name="bcc"
-                                    value={values.subject}
-                                    onChange={handleChange}
-                                    isInvalid={!!errors.subject}
-                                />
-
-                                <Form.Control.Feedback type="invalid">
-                                    {errors.subject}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
+        <>
+          <Formik
+              validationSchema={schema}
+              onSubmit={(values) => {setFormResponse("Success!")}}
+              initialValues={{
+                  mailTo: '',
+                  cc: '',
+                  bcc: '',
+                  subject: '',
+                  body: '',
+              }}
+          >
+              {({
+                  handleSubmit,
+                  handleChange,
+                  handleBlur,
+                  values,
+                  touched,
+                  isValid,
+                  errors,
+              }) => (
+                  <Form noValidate onSubmit={handleSubmit}>
+                    <Form.Group as={Col} md="4" controlId="validationFormik01">
+                        <Form.Label>Mail to</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="mailTo"
+                            value={values.mailTo}
+                            onChange={handleChange}
+                            isInvalid={!!errors.mailTo}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          {errors.mailTo}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="4" controlId="validationFormik02">
+                        <Form.Label>cc</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="cc"
+                            value={values.cc}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} md="4" controlId="validationFormikUsername">
+                        <Form.Label>bcc</Form.Label>
                         <Form.Group as={Col} md="6" controlId="validationFormik03">
-                            <Form.Label>subject</Form.Label>
+                            <Form.Label>bcc</Form.Label>
                             <Form.Control
                                 type="text"
                                 placeholder=""
-                                name="subject"
-                                value={values.subject}
+                                name="bcc"
+                                value={values.bcc}
                                 onChange={handleChange}
-                                isInvalid={!!errors.subject}
                             />
-
-                            <Form.Control.Feedback type="invalid">
-                                {errors.subject}
-                            </Form.Control.Feedback>
                         </Form.Group>
-                        <Form.Group as={Col} md="3" controlId="validationFormik04">
-                            <Form.Label>body</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder=""
-                                name="body"
-                                value={values.body}
-                                onChange={handleChange}
-                                isInvalid={!!errors.body}
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                {errors.body}
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                    </Form.Row>
-                    <Button type="submit">Submit form</Button>
-                </Form>
-            )}
-        </Formik>
+                    </Form.Group>
+                    <Form.Group as={Col} md="6" controlId="validationFormik03">
+                        <Form.Label>subject</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            name="subject"
+                            value={values.subject}
+                            onChange={handleChange}
+                            isInvalid={touched.subject && !!errors.subject}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.subject}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="3" controlId="validationFormik04">
+                        <Form.Label>body</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            name="body"
+                            value={values.body}
+                            onChange={handleChange}
+                            isInvalid={touched.body && !!errors.body}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.body}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                      <Button type="submit">Submit form</Button>
+                  </Form>
+              )}
+          </Formik>
+          <p>{formResponse}</p>
+      </>
     )
 }
