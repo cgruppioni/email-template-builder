1b0d3ac239878db090fab983c87b5a9d3695f5f1
Casey Gruppioni <cgruppioni@gmail.com>
2020-09-14 15:04:21 -0400

    Updates

---
+++ precache-manifest.ec73476d36bb24386889db2dd4680243.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd860d5f5889bddecc7953c326b3072",
+    "url": "/email-template-builder/index.html"
+  },
+  {
+    "revision": "a57ce09b8ad44e206428",
+    "url": "/email-template-builder/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "bc1ddd6c4b17e2993ba6",
+    "url": "/email-template-builder/static/css/main.f9792255.chunk.css"
+  },
+  {
+    "revision": "a57ce09b8ad44e206428",
+    "url": "/email-template-builder/static/js/2.cab3d754.chunk.js"
+  },
+  {
+    "revision": "5b2153c92f41e6a95aa4e76fa8069531",
+    "url": "/email-template-builder/static/js/2.cab3d754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1ddd6c4b17e2993ba6",
+    "url": "/email-template-builder/static/js/main.ddf8aca6.chunk.js"
+  },
+  {
+    "revision": "eaa459c587c52f1b8b6e",
+    "url": "/email-template-builder/static/js/runtime-main.9b078044.js"
+  },
+  {
+    "revision": "f54212c17a6d61e39e480c3bd76d9b43",
+    "url": "/email-template-builder/static/media/mail.f54212c1.svg"
+  }
+]);
